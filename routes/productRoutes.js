70a5ebc0c1d29c6b30b0cb7095e8aecf14400ed1
smurfish-py <m.rifki0729@gmail.com>
2025-09-18 21:54:50 +0700

    Adding product routing file

---
+++ routes/productRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient();
+const router = express.Router();
+
+router.post('/', (req, res) => {
+    try {
+        
+    } catch (error) {
+
+    }
+});
