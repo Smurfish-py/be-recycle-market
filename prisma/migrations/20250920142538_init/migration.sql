4e663cafa4b982776f978b06dc34456472561038
Smurfish-py <m.rifki0729@gmail.com>
2025-09-22 10:39:53 +0700

    Update migration.sql

--- prisma/migrations/20250920142538_init/migration.sql
+++ prisma/migrations/20250920142538_init/migration.sql
@@ -1,22 +1,15 @@
-/*
-  Warnings:
-
-  - You are about to drop the `Test` table. If the table is not empty, all the data it contains will be lost.
-
-*/
--- DropTable
-DROP TABLE `Test`;
-
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `fullname` VARCHAR(191) NOT NULL,
     `username` VARCHAR(30) NOT NULL,
     `email` VARCHAR(191) NOT NULL,
     `password` VARCHAR(191) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-    `lastOnline` DATETIME(3) NOT NULL,
-    `status` VARCHAR(191) NOT NULL,
+    `lastOnline` DATETIME(3) NULL,
+    `status` ENUM('ACTIVE', 'BANNED') NOT NULL DEFAULT 'ACTIVE',
 
+    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
