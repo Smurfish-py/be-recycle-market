acde784a4a1bb91e43ca0bba410b4bfa9709b19e
smurfish-py <m.rifki0729@gmail.com>
2025-09-24 22:23:47 +0700

    Refactor API routes to include versioning in endpoint paths

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const user = require("./routes/userRoutes");
+const produk = require("./routes/productRoutes");
+const toko = require("./routes/tokoRoutes");
 
 const PORT = 3000;
 const app = express();
 
 app.use(express.json());
-app.use('/users', user);
+
+app.use('/api/user', user);
+app.use('/api/produk', produk);
+app.use('/api/toko', toko);
 
 app.listen(PORT, () => {
     console.log(`Server berjalan di http://localhost:${PORT}`);
