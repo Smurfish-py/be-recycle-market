0f36a028ddb8eac8b7f126f2d983ab45e030a388
smurfish-py <m.rifki0729@gmail.com>
2025-09-25 21:01:46 +0700

    Completing  routes

--- routes/tokoRoutes.js
+++ routes/tokoRoutes.js
@@ -33,7 +33,7 @@ router.get('/data/:id', async (req, res) => {
 });
 
 router.post('/create', async (req, res) => {
-    const { namaToko, deskripsi, idUser} = req.body;
+    const { nama, deskripsi, idUser} = req.body;
     try {
         const checkToko = await prisma.toko.findUnique({
             where: { 
@@ -44,15 +44,59 @@ router.post('/create', async (req, res) => {
         if (!checkToko) {
             await prisma.toko.create({
                 data: {
-                    namaToko: namaToko,
+                    nama: nama,
                     deskripsi: deskripsi,
                     idUser: idUser
                 }
             });
-
             res.json({ message: "Toko telah berhasil di daftarkan!" });
         } else {
             return res.status(400).json({ message: 'Toko sudah terdaftar' });
+        }
+    } catch (error) {
+        console.error({ error: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.patch('/update/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const checkToko = await prisma.toko.findUnique({
+            where: { id: id }
+        });
+
+        if (checkToko) {
+            await prisma.toko.update({
+                where: { id: Number(id) },
+                data: req.body
+            });
+            res.json({ message: "Perubahan disimpan!" });
+        } else {
+            res.status(404).json({ message: "Toko tidak ditemukan!" });
+        }
+    } catch (error) {
+        console.error({ error: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const checkToko = await prisma.toko.findUnique({
+            where: { id: Number(id) }
+        });
+
+        if (checkToko) {
+            await prisma.toko.delete({
+                where: { id: Number(id) }
+            });
+            res.json({ message: "Toko berhasil dihapus" });
+        } else {
+            res.status(404).json({ message: "Toko tidak ditemukan" });
         }
     } catch (error) {
         console.error({ error: error });
