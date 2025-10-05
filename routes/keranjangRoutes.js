0cc418c261f6ede145123f7a56403f24ea4a4dc9
smurfish-py <m.rifki0729@gmail.com>
2025-10-05 07:49:32 +0700

    Finishing the routes

--- routes/keranjangRoutes.js
+++ routes/keranjangRoutes.js
@@ -4,12 +4,59 @@ const { PrismaClient } = require('@prisma/client');
 const router = express.Router();
 const prisma = new PrismaClient();
 
-router.get('/', async (req, res) => {
+router.get('/:id', async (req, res) => {
+    const { id } = req.params;
     try {
-        const daftarKeranjang = await prisma.keranjang.findMany();
+        const daftarKeranjang = await prisma.keranjang.findMany({
+            where: {
+                idUser: Number(id),
+            },
+        });
         res.json(daftarKeranjang);
     } catch (error) {
         console.error({ error: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.post('/add', async (req, res) => {
+    const data = req.body;
+    
+    try {
+        await prisma.keranjang.create({
+            data: data
+        });
+        res.json({ message: "data berhasil ditambahkan" });
+    } catch (error) {
+        console.error({ error: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete', async (req, res) => {
+    try {
+        await prisma.keranjang.deleteMany();
+        res.json({ message: "Keranjang berhasil dikosongkan" });
+    } catch (error) {
+        console.error({ errpr: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        await prisma.keranjang.delete({
+            where: {
+                idProduk: Number(id)
+            }
+        });
+        res.json({ message: "Keranjang berhasil dikosongkan" });
+    } catch (error) {
+        console.error({ errpr: error });
     } finally {
         prisma.$disconnect();
     }
