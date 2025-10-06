e35de1184216b8e9562ac01d8d0a6cb577bebdd0
smurfish-py <m.rifki0729@gmail.com>
2025-10-06 09:58:22 +0700

    Update: backend completion for penjualan & transaksi

--- routes/historiPenjualanRoutes.js
+++ routes/historiPenjualanRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const { PrismaClient } = require('@prisma/client');
-const { route } = require('./keranjangRoutes');
 
 const router = express.Router();
 const prisma = new PrismaClient();
@@ -8,7 +7,7 @@ const prisma = new PrismaClient();
 router.get('/:id', async (req, res) => {
     const { id } = req.params;
     try {
-        const user = prisma.user.findUnique({
+        const user = await prisma.user.findUnique({
         where: {
             id: Number(id)
             }
@@ -16,7 +15,7 @@ router.get('/:id', async (req, res) => {
 
         if (!user) return res.status(404).json({ message:"Pengguna tidak ditemukan" });
 
-        const produk = prisma.penjualan.findMany({
+        const produk = await prisma.penjualan.findMany({
             where: {
                 idUser: Number(id)
             }
@@ -48,12 +47,68 @@ router.post('/add', async (req, res) => {
     }
 });
 
-router.patch('/update/:id', async (req, res) => {
-    const { id } = req.params;
+router.patch('/update/user/:id/item/:itemId', async (req, res) => {
+    const { id, itemId } = req.params;
+    const data = req.body;
     try {
-        
+        const updateHistori = await prisma.penjualan.update({
+            where: {
+                idUser: Number(id),
+                idProduk: Number(itemId)
+            },
+            data: data
+        });
+
+        if (!updateHistori) return res.status(400).json({ message: "Gagal mengubah histori" });
+
+        res.status(201).json({ message: "Data berhasil diubah" });
     } catch (error) {
         console.error({ error: error });
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete', async (req, res) => {
+    try {
+        const hasil = await prisma.penjualan.deleteMany();
+
+        if (!hasil) return res.status(400).json({ message: "Gagal menghapus data" });
+
+        res.status(201).json({ message: "Berhasil menghapus data" });
+    } catch (error) {
+        console.error(error);
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete/user/:id/item/:itemId', async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+        const checkPenjualan = await prisma.penjualan.findMany({
+            where: {
+                idUser: Number(id),
+                idProduk: Number(itemId)
+            }
+        });
+
+        if (!checkPenjualan) {
+            return res.status(404).json({ message: "History tidak ditemukan" });
+        } else {
+            const hapusHistori = prisma.penjualan.deleteMany({
+                where: {
+                    idUser: Number(id),
+                    idProduk: Number(itemId)
+                }
+            });
+
+            if (!hapusHistori) return res.status(400).json({ message: "Gagal menghapus histori" });
+
+            res.status(201).json({ message: "Histori berhasil dihapus" });
+        }
+    } catch (error) {
+        console.error(error);
     } finally {
         prisma.$disconnect();
     }
--- routes/historiTransaksiRoutes.js
+++ routes/historiTransaksiRoutes.js
@@ -35,4 +35,80 @@ router.post('/add', async (req, res) => {
     }
 });
 
+router.patch("/update/user/:id/item/:itemId", async (req, res) => {
+    const { id, itemId } = req.params;
+    const data = req.data;
+    try {
+        const checkUser = await prisma.user.findUnique({
+            where: {
+                id: Number(id)
+            }
+        });
+
+        if (!checkUser) {
+            return res.status(404).json({ message: "User tidak ditemukan" })
+        } else {
+            const hasil = await prisma.transaksi.update({ 
+                where: {
+                    idUser: id,
+                    idProduk: itemId
+                },
+                data: data
+            });
+            if (!hasil) return res.status(400).json({ message: "Gagal update user" });
+
+            res.status(201).json({ message: "Data berhasil diubah" });
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete', async (req, res) => {
+    try {
+        const hasil = await prisma.transaksi.deleteMany();
+
+        if (!hasil) return res.status(400).json({ message: "Gagal menghapus history" });
+
+        res.status(201).json({ message: "History berhasil dihapus" });
+    } catch (error) {
+        console.error(error);
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
+router.delete('/delete/user/:id/item/:itemId', async (req, res) => {
+    const { id, itemId } = req.params;
+    try {
+        const checkTransaksi = await prisma.transaksi.findMany({
+            where: {
+                idUser: Number(id),
+                idProduk: Number(itemId)
+            }
+        });
+
+        if (!checkTransaksi) {
+            return res.status(404).json({ message: "History tidak ditemukan" });
+        } else {
+            const hapusHistori = prisma.transaksi.deleteMany({
+                where: {
+                    idUser: Number(id),
+                    idProduk: Number(itemId)
+                }
+            });
+
+            if (!hapusHistori) return res.status(400).json({ message: "Gagal menghapus histori" });
+
+            res.status(201).json({ message: "Histori berhasil dihapus" });
+        }
+    } catch (error) {
+        console.error(error);
+    } finally {
+        prisma.$disconnect();
+    }
+});
+
 module.exports = router;
