7ac5e8d3dfcec2c548169754c47b1026f9facb55
smurfish-py <m.rifki0729@gmail.com>
2025-10-11 20:47:45 +0700

    Importing app and using dotenv

--- server.mjs
+++ server.mjs
@@ -1,25 +1,8 @@
-import express from 'express';
-import user from './routes/userRoutes.js';
-import produk from './routes/productRoutes.js';
-import toko from './routes/tokoRoutes.js';
-import keranjang from './routes/keranjangRoutes.js';
-import transaksi from './routes/historiTransaksiRoutes.js';
-import penjualan from './routes/historiPenjualanRoutes.js'
-
+import app from './src/app.mjs'
 import chalk from 'chalk';
+import 'dotenv/config'
 
-const PORT = 3000;
-const app = express();
-
-app.use(express.json());
-
-app.use('/api/user', user);
-app.use('/api/produk', produk);
-app.use('/api/toko', toko);
-app.use('/api/keranjang', keranjang);
-app.use('/api/transaksi', transaksi);
-app.use('/api/penjualan', penjualan);
-
+let PORT = process.env.PORT;
 
 app.listen(PORT, () => {
     console.log(`
