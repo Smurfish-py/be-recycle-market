d35fc8a522439a58dd0b5f948d266ca779d1804b
smurfish-py <m.rifki0729@gmail.com>
2025-10-26 20:18:36 +0700

    Refactor: update CORS configuration to use environment variable for origin and add path handling

--- src/app.mjs
+++ src/app.mjs
@@ -1,5 +1,7 @@
 import express from 'express';
-import cors from 'cors'; // => Cors berfungsi
+import cors from 'cors';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
 
 import user  from './routes/userRoutes.js';
 import produk from './routes/productRoutes.js';
@@ -7,12 +9,16 @@ import toko from './routes/tokoRoutes.js';
 import keranjang from './routes/keranjangRoutes.js';
 import transaksi from './routes/historiTransaksiRoutes.js';
 import penjualan from './routes/historiPenjualanRoutes.js'
+import path from 'path';
 
 const app = express();
+const WEB_URL = process.env.WEB_URL.split(",");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 app.use(express.json());
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: WEB_URL,
     methods: ["GET", "POST", "PATCH", "DELETE"]
 }));
 
@@ -22,5 +28,7 @@ app.use('/api/toko', toko);
 app.use('/api/keranjang', keranjang);
 app.use('/api/transaksi', transaksi);
 app.use('/api/penjualan', penjualan);
+app.use('/api/images/products', express.static(path.join(__dirname, 'img', 'products')));
+app.use('/api/images/users', express.static(path.join(__dirname, 'img', 'products')));
 
 export default app;
