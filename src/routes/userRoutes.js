8d9e25e2c0c0442d615a09c9a73fa69bb83fc3c8
smurfish-py <m.rifki0729@gmail.com>
2025-11-06 14:05:10 +0700

    adding login and register endpoint

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,83 @@
 import express from 'express';
 import { PrismaClient } from '@prisma/client';
+import jwt from 'jsonwebtoken';
+import bcrypt, { hash } from 'bcryptjs';
 
 const router = express.Router();
 const prisma = new PrismaClient();
+const JWT_SECRET = process.env.JWT_SECRET;
 
+router.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            },
+            include: {
+                privilege: {
+                    select: {
+                        privilege: true
+                    }
+                }
+            }
+        });
+
+        if (!user) return res.status(404).json({ pesan: `Tidak ada user dengan email ${email}` });
+        if (!bcrypt.compare(password, user.password)) return res.json({ pesan: 'Password anda salah!' });
+
+        const token = jwt.sign({
+            id: user.id,
+            email,
+            password,
+            privilege: user.privilege[0]?.privilege || null
+        },
+        JWT_SECRET,
+        { expiresIn: '1h' });
+
+        return res.json({ token });
+    } catch (error) {
+        throw error;
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const { fullname, email, password } = req.body;
+
+    try {
+
+        const user = await prisma.user.findUnique({
+            where: {
+                email
+            }
+        });
+        
+        if (user) return res.status(409).json({ status: false, pesan: `Email ${email} sudah terdaftar, gunakan email lain` });
+
+        const hashed = await bcrypt.hash(password, 10);
+        
+        const addUser = await prisma.user.create({
+            data: {
+                fullname,
+                username: fullname,
+                password: hashed,
+                email,
+                privilege: {
+                    create: {
+                        privilege: 'default'
+                    }
+                }
+            }
+        });
+
+        if (!addUser) return res.json({ status: true, pesan: "Gagal mendaftarkan" });
+
+        return res.status(201).json({ status: true, pesan: "Pendaftaran berhasil, silakan login!" });
+
+    } catch (error) {
+        throw error;
+    }
+});
 
 router.get('/', async (req, res) => {
     try {
