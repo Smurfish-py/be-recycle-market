ead57f3e06cf0a56fd74424ed03b57cbd81ab1e1
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-03-14 09:55:48 +0700

    feat: adding seed.js for adding admin account

---
+++ prisma/seed.mjs
@@ -0,0 +1,27 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const prisma = new PrismaClient();
+
+async function main() {
+    const hashedPassword = await bcrypt.hash('admin123', 10);
+        const admin = await prisma.user.upsert({
+            where: { email: 'admin@recycle.com' },
+            update: {},
+            create: {
+            fullname: 'Super Admin',
+            username: 'admin',
+            email: 'admin@recycle.com',
+            password: hashedPassword,
+            privilege: {
+                create: { privilege: 'ADMIN' }
+            }
+        }
+    });
+
+    console.log({ admin });
+}
+
+main()
+    .catch((e) => console.error(e))
+    .finally(async () => await prisma.$disconnect());
